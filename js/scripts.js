39f9246d9d06c657739828dc593a521f744abfa0
Nick Tesh <56547918+nicktesh@users.noreply.github.com>
2021-07-30 16:47:32 -0400

    Added User Add Suggestion
    Working on upvoting and downvoting next.

--- js/scripts.js
+++ js/scripts.js
@@ -55,10 +55,129 @@ if (document.getElementById("feedback-page")) {
 }
 
 //
-// Allows website users to add feedback //
+// Allows users to add Suggestions //
 //
 
-let feedbackBtn = document.getElementById("feedbackBtn");
-let feedbackModal = document.getElementById("feedbackModal");
+let allFeedbackPosts = document.getElementById("allFeedbackPosts");
+let feedback = document.createElement("div");
+let feedbackForm = document.getElementById("feedbackForm");
+let title = document.getElementById("feedbackTitleInput");
+let description = document.getElementById("feedbackDescInput");
+let category = document.getElementById("feedbackCategoryInput");
+// Adds user added feedback to localStorage and prepends current storage to top of parent div
+feedback.innerHTML = localStorage.getItem("newFeedback");
+allFeedbackPosts.prepend(feedback);
 
-feedbackBtn.addEventListener("click", () => {});
+// When the user fills out the feedback form
+feedbackForm.addEventListener("submit", (event) => {
+  // If any of the inputs are blank, gives error message
+  event.preventDefault();
+  if (
+    title.value === "" ||
+    description.value === "" ||
+    category.value === "Select a Category"
+  ) {
+    alert("Fix this NOW");
+    // If all inputs are successful, it creates the feedback box and prepends it
+  } else {
+    // Sets the filter data as the category selected, and makes it lowercase so the filter works
+    let feedbackType = category.value.toLowerCase();
+    // This creates a div for every object in the JSON data and adds it to the top of the #allFeedbackPosts row
+    feedback.innerHTML = `<div class="feedback-post-box" data-filter="${feedbackType}">
+    <div class="post-box-inner">
+      <div class="total-count-box">
+        <div class="total-count-inner">
+          <img src="assets/img/chevron-up-solid.svg">
+          <p>0</p>
+        </div>
+      </div>
+      <div class="post-info">
+        <h3>${title.value}</h3>
+        <p>${description.value}</p>
+        <div class="post-category">${category.value}</div>
+      </div>
+      <div class="post-comments">
+        <img src="assets/img/comment-alt-solid.svg">
+        <p>0</p>
+      </div>
+      <button class="delete-btn">X</button>
+    </div>
+  </div>`;
+    allFeedbackPosts.prepend(feedback);
+
+    // Resets the form
+    $("#feedbackForm")[0].reset();
+
+    // Hides the modal after clicking submit
+    $("#feedbackModal").modal("toggle");
+  }
+
+  let deleteBtn = document.querySelectorAll(".delete-btn");
+
+  for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener("click", () => {
+      // Removes the entire div and contents created from feedbackForm
+      deleteBtn[i].parentElement.parentElement.parentElement.remove();
+      // Removes the selected feedback box from localStorage
+      localStorage.removeItem("newFeedback", feedback.innerHTML);
+    });
+  }
+
+  // Stores the feedback box into localStorage
+  localStorage.setItem("newFeedback", feedback.innerHTML);
+});
+
+//
+// Allows the user to delete the feedback that they added //
+//
+let deleteBtn = document.querySelectorAll(".delete-btn");
+
+for (let i = 0; i < deleteBtn.length; i++) {
+  deleteBtn[i].addEventListener("click", () => {
+    // Removes the entire div and contents created from feedbackForm
+    deleteBtn[i].parentElement.parentElement.parentElement.remove();
+    // Removes the selected feedback box from localStorage
+    localStorage.removeItem("newFeedback", feedback.innerHTML);
+  });
+}
+
+//
+// Allows users to upvote and downvote suggestions
+//
+
+let totalCountInner = document.querySelectorAll(".total-count-inner");
+let totalVotes = document.querySelectorAll(".totalVotes");
+
+for (let i = 0; i < totalCountInner.length; i++) {
+  totalCountInner[i].addEventListener("click", () => {
+    for (let j = 0; j < totalVotes.length; j++) {
+      let count = Number(totalVotes[j].innerHTML);
+
+      if (count === count) {
+        totalVotes[j].innerHTML = count + 1;
+      } else {
+        return false;
+      }
+    }
+  });
+}
+
+// let totalCountInner = document.querySelectorAll(".total-count-inner");
+// let totalVotes = document.querySelectorAll(".totalVotes");
+
+// totalVotes.forEach(function (i) {
+//   i.addEventListener("click", function () {
+//     console.log(i);
+//   });
+// });
+
+// for (i of totalCountInner) {
+//   let totalValue = Number(totalVotes.innerHTML);
+//   i.addEventListener("click", () => {
+//     if (totalValue === totalValue) {
+//       totalVotes.innerHTML = totalValue + 1;
+//     } else {
+//       return false;
+//     }
+//   });
+// }
